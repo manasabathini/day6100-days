bd1dd4bf96f99c5144621513d4182a46564cb187
manasabathini <33703157+manasabathini@users.noreply.github.com>
2023-04-25 22:23:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Welcome to Login page")
+print("Please follow the instructions to login")
+userName = input("Please enter username: ")
+password = input("Please enter password: ")
+
+if userName == "John" and password == "password":
+  print("Welcome", userName)
+elif userName == "Nick" and password == "password":
+  print("Welcome", userName)
+elif userName == "Melissa" and password == "password":
+  print("Welcome", userName)
+elif userName == "Shan" and password == "password":
+  print("Welcome", userName)
+else:
+  print("Please enter user credentials")
